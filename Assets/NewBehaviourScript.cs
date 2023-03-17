7cd771b607a1772f548ada625ff7827b11fbc4a5
bdsouza1 <bdsouza1@nait.ca>
2023-03-17 09:18:48 -0600

    commit ex 2

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField] int targetScene;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,13 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            GoToTarget(targetScene);
+        }
+    }
+    public void GoToTarget(int scene)
+    {
+        SceneManager.LoadScene(scene);
     }
 }
